7a6b257ccfe382c04f5a54dd1d67021a587d87cf
James Moughan <jamougha@gmail.com>
2015-03-31 22:43:34 +0100

    Find the most common words and save/load the to/from a text file. Add basic impl of a word vector.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,105 @@
+#![feature(std_misc)]
+
+use std::fs::{File, read_dir};
+use std::io::{BufReader, BufRead, Read, Write};
+use std::path::Path;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry::*;
+use std::ascii::OwnedAsciiExt;
+
+fn count_tokens<T: Read>(reader: BufReader<T>, word_counts: &mut HashMap<String, u32>) {
+
+    for line in reader.lines().skip(30) {
+    	if let Ok(line) = line {
+	    	for word in line.split(|c: char| !c.is_alphabetic()) {
+	    		if word.len() > 0 {
+	    			let word = word.to_string().into_ascii_lowercase();
+		    		match word_counts.entry(word) {
+		    			Occupied(mut e) => { *e.get_mut() += 1; },
+		    			Vacant(e) => { e.insert(1); }
+		    		}
+		    	}
+	    	}
+	    }
+    }
+}
+
+fn find_most_common_words(corpus_loc: &str, outfile: &str) {
+    let path = Path::new(corpus_loc);
+    let files = read_dir(path);
+    let mut word_counts = HashMap::new();
+
+    for file in files.unwrap() {
+    	let path = file.unwrap().path();
+    	if path.to_str().unwrap().ends_with(".txt") {
+		    let file = File::open(&path).unwrap();
+		    let reader = BufReader::new(file);
+    		count_tokens(reader, &mut word_counts);
+    		
+    	}
+	    println!("{:?}", word_counts.len());
+    }
+
+    let mut counts: Vec<_> = word_counts.into_iter().collect();
+    counts.sort_by(|a, b| b.1.cmp(&a.1));
+
+    let mut out = File::create(&Path::new(outfile)).unwrap();
+    for &(ref word, count) in &counts[0..10000] {
+        out.write_all(format!("{}, {}\n", word, count).as_bytes()).unwrap();
+    }
+    
+    println!("{:?}", &counts[0..20]);
+    println!("{:?}", &counts[counts.len() - 20..counts.len()]);
+}
+
+fn load_most_common_words(filename: &str) -> Vec<String> {
+    let file = File::open(&Path::new(filename)).unwrap();
+    let reader = BufReader::new(file);
+    reader.lines().map(|line| 
+        line.unwrap().split(',').next().unwrap().to_string()
+    ).collect()
+}
+
+struct WordVec {
+    word: String,
+    count: u32,
+    normalized: bool,
+    vec: Box<[f32; 10_000]>,
+}
+
+impl WordVec {
+    fn new(word: String) -> WordVec {
+        WordVec {
+            word: word,
+            count: 0,
+            normalized: false,
+            vec: Box::new([0f32; 10_000]),
+        }
+    }
+
+    fn normalize(&mut self) {
+        assert!(!self.normalized);
+        for elem in &mut self.vec[..] {
+            *elem /= self.count as f32;
+        }
+        self.normalized = true;
+    }
+
+    fn inc(&mut self, i: usize) {
+        self.vec[i] += 1.0;
+    }
+
+    fn distance(&self, other: &WordVec) -> f32 {
+        self.vec.iter()
+                .zip(other.vec.iter())
+                .map(|(x, y)| (x*x - y*y).sqrt())
+                .fold(0.0, |x, y| x + y)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // find_most_common_words("/home/jamougha/corpus/pg", "/home/jamougha/corpus/pg/word_counts.csv");
+    let words = load_most_common_words("/home/jamougha/corpus/pg/word_counts.csv");
+    println!("{:?}", words);
+
 }
